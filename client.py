96c2ffff216262144c97dd9caf54387e754cc05a
XxGtocxX <debanuj.de.official@gmail.com>
2026-04-06 19:56:33 +0530

    Remove unused client wrapper

--- client.py
+++
@@ -1,9 +0,0 @@
-from openenv.core import EnvClient
-from models import TravelAction, TravelObservation
-
-class TravelEnv(EnvClient):
-    """
-    Client for interacting with the Travel Pro environment server.
-    Inherits from EnvClient to provide standard API connectivity.
-    """
-    pass
